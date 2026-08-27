b877c1565910f74d3498eef39ae0c1dc6e4ddf67
shen <ixenbrandonestiben@gmail.com>
2026-08-27 13:51:58 -0600

    :sparkles: feat(dql) creacion de nueva tabla llamada datos_fake

--- tienda/dql/consultas.sql
+++ tienda/dql/consultas.sql
@@ -4,3 +4,13 @@ VALUES (100, 'producto fake', 5, 0);
 -- eliminacion en base a consulta.
 DELETE FROM productos
  WHERE id NOT IN (SELECT producto_id FROM ventas) AND stock = 0;
+
+
+
+CREATE TABLE datos_fake(
+    id SERIAL PRIMARY KEY,
+    codigo CHAR(3),
+    nombre VARCHAR(30),
+    apellido VARCHAR(30),
+    edad INT
+);
