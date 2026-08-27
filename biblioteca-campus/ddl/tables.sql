f6f70467e2fda68c5bfa9fad810109024a8a2e80
shen <ixenbrandonestiben@gmail.com>
2026-08-26 21:54:22 -0600

    :sparkles: feat(ddl) creacion tabla libros

--- biblioteca-campus/ddl/tables.sql
+++ biblioteca-campus/ddl/tables.sql
@@ -5,3 +5,12 @@ apellido VARCHAR(30) NOT NULL,
 fecha_nacimiento DATE,
 nacionalidad VARCHAR(50)
 );
+
+CREATE TABLE libros (
+libro_id SERIAL PRIMARY KEY,
+autor_id INT NOT NULL,
+titulo VARCHAR(255) NOT NULL,
+isbn VARCHAR(20) UNIQUE,
+fecha_publicacion DATE,
+precio NUMERIC(10, 2)
+);
