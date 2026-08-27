5aa4b9c42bf3b418b9c4c97457149d44ca131005
shen <ixenbrandonestiben@gmail.com>
2026-08-26 21:57:54 -0600

    :sparkles: feat(dml) datos agregados a la tabla autores

---
+++ biblioteca-campus/dml/inserts.sql
@@ -0,0 +1,12 @@
+INSERT INTO autores (nombre, apellido, fecha_nacimiento, nacionalidad)
+VALUES
+('Gabriel', 'García Márquez', '1927-03-06', 'Colombiana'),
+('Isabel', 'Allende', '1942-08-02', 'Chilena'),
+('Jorge Luis', 'Borges', '1899-08-24', 'Argentina'),
+('Mario', 'Vargas Llosa', '1936-03-28', 'Peruana'),
+('Julio', 'Cortázar', '1914-08-26', 'Argentina'),
+('Laura', 'Esquivel', '1950-09-30', 'Mexicana'),
+('Carlos', 'Ruiz Zafón', '1964-09-25', 'Española'),
+('Octavio', 'Paz', '1914-03-31', 'Mexicana'),
+('Rosa', 'Montero', '1951-01-03', 'Española'),
+('Arturo', 'Pérez-Reverte', '1951-11-25', 'Española');
