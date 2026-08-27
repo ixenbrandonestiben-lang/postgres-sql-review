da1281b0e0edcc747155c79d0b223aba624527cf
shen <ixenbrandonestiben@gmail.com>
2026-08-27 13:53:04 -0600

    :sparkles: feat(dql) insercion de datos a la tabla datos_fake

--- tienda/dql/consultas.sql
+++ tienda/dql/consultas.sql
@@ -14,3 +14,16 @@ CREATE TABLE datos_fake(
     apellido VARCHAR(30),
     edad INT
 );
+
+INSERT INTO datos_fake(codigo, nombre, apellido, edad) VALUES
+('001', 'Patricia', 'Fernandez', 40),
+('002', 'Armando', 'Benedetti', 38),
+('001', 'Patricia', 'Fernandez', 40),
+('003', 'Kevin', 'Garcia', 42),
+('004', 'Jhon', 'Cuevas', 23),
+('005', 'Luna', 'Herrera', 40),
+('002', 'Armando', 'Benedetti', 38),
+('001', 'Patricia', 'Fernandez', 40),
+('002', 'Armando', 'Benedetti', 38),
+('001', 'Patricia', 'Fernandez', 40);
+
