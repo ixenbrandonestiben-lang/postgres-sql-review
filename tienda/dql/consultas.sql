ce8b0ec19bc07c16326b1409c600795925743dc2
shen <ixenbrandonestiben@gmail.com>
2026-08-27 13:50:39 -0600

    :sparkles: feat(dql) eliminacion en base a consultas

--- tienda/dql/consultas.sql
+++ tienda/dql/consultas.sql
@@ -1,2 +1,6 @@
 INSERT INTO productos(id, nombre, precio, stock)
 VALUES (100, 'producto fake', 5, 0);
+
+-- eliminacion en base a consulta.
+DELETE FROM productos
+ WHERE id NOT IN (SELECT producto_id FROM ventas) AND stock = 0;
